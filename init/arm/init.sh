80dd7a1405d21070d49513d8208c5962f0802d74
Joshua Dong <jdong42@gmail.com>
2021-05-11 17:47:20 -0500

    updates

--- init/arm/init.sh
+++ init/arm/init.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-ssh root@10.0.0.11 'sh -s' < init_keyring_populate.sh
-ssh root@10.0.0.11 'sh -s' < python.sh
+HOST="root@10.0.0.11"
+ssh-copy-id "${HOST}"
+ssh "${HOST}" 'sh -s' < init_keyring_populate.sh
+ssh "${HOST}" 'sh -s' < python.sh
